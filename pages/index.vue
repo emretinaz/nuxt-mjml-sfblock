98d0dc8570d678b12d111015ce44388ffd01b246
Emre TINAZ <emre.tinaz@trivago.com>
2021-01-21 17:17:02 +0100

    initial

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Welcome to Nuxt-Mjml-SFBlocks</h1>
+      <br /><br />
+      <nuxt-link to="/trv-logo-header-v2"><button>trv-logo-header-v2</button></nuxt-link>
+      <br>
+      <br>
+      <nuxt-link to="/trv-headline-v2"><button>trv-headline-v2</button></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+</style>
---
+++ pages/trv-headline-v2/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div>
+      <h2>trv-headline-v2</h2>
+      <br> <br>
+      <nuxt-link to="/"><button>Home</button></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+</style>
